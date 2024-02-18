0d9e823b168c759fa7c2a7f2c34dabb890f201ca
Suryansh <suryanshgupta6502@gmail.com>
2024-02-18 14:29:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const cors = require("cors")
+const mongoose = require("mongoose")
+require('dotenv').config()
+
+
+
+const app = express()
+app.use(cors())
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB)
+    .then(() => {
+        console.log("connected");
+    }).catch((err) => {
+        console.log("error in coneciton", err);
+    })
+
+const userschema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    device: {
+        type: String
+    },
+    rawData: {
+        type: Array
+    }
+}, {
+    timestamps: {
+        createdAt: new Date().toString(),
+        updatedAt: false
+    }
+}
+)
+
+
+const usermodal = mongoose.model("donutWorld/User", userschema)
+
+
+app.post('/donutWorld', async (req, res) => {
+    // console.log(req.rawHeaders);
+    const { name, device } = req.body;
+    const user = await usermodal({ name: name, device: device, rawData: req.rawHeaders }).save()
+    // console.log(user);
+    res.send("donut world")
+})
+
+
+
+
+
+app.listen(process.env.PORT||3000)
